40b0eb2ab88b80c0d8a2c2016b958e4af62cf6d9
Rahul Bharati <rahulrbharati@gmail.com>
2022-02-13 00:53:42 +0530

    1. Sign In to App 2. Sign out of app

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,10 @@
 import type { NextComponentType } from "next";
+import { useContext } from "react";
+import { AppContext } from "./../context/AppContext";
 
 const Navbar: NextComponentType = () => {
+  const { signIn, user, signOut } = useContext(AppContext);
+
   return (
     <div className="w-full fixed top-0 left-0 bg-gray-900 z-10 shadow-md">
       <div className="container mx-auto max-w-[1200px] p-4 flex items-center justify-between">
@@ -10,11 +14,25 @@ const Navbar: NextComponentType = () => {
         <ul className="list-none text-white flex items-center justify-center">
           <li className="text-xl mx-2">Write</li>
           <li className="text-xl mx-2">Stories</li>
-          <li className="text-xl ml-4">
-            <button className="btn px-8 py-2 text-xl text-white bg-blue-600 rounded-full">
-              Sign in
-            </button>
-          </li>
+          {!user ? (
+            <li className="text-xl ml-4">
+              <button
+                className="btn px-8 py-2 text-xl text-white bg-blue-600 rounded-full"
+                onClick={() => signIn()}
+              >
+                Sign in
+              </button>
+            </li>
+          ) : (
+            <li className="text-xl ml-4">
+              <button
+                className="btn px-8 py-2 text-xl text-white bg-blue-600 rounded-full"
+                onClick={() => signOut()}
+              >
+                Sign out
+              </button>
+            </li>
+          )}
         </ul>
       </div>
     </div>
